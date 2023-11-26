0abbbe0cc1255afb7dfcd18d6c56a0487c1a4815
terbik <terbik123255@gmail.com>
2023-11-26 13:15:19 +0300

    some_repeat

--- Category_view.py
+++ Category_view.py
@@ -17,8 +17,7 @@ class CategoryData(QtWidgets.QMainWindow):
         self.tableView = self.findChild(QtWidgets.QTableView, 'tableView')
         self.table_model = QStandardItemModel()
         self.tableView.setModel(self.table_model)
-        self.pushButton.hide()
-
+        self.pushButton.clicked.connect(self.add_new_category)
 
         conn = Model().conn
         cur = conn.cursor()
@@ -31,6 +30,21 @@ class CategoryData(QtWidgets.QMainWindow):
         self.load_data(data, column_data)
 
         self.pushButton_3.clicked.connect(self.exit)
+
+        self.tableView.doubleClicked.connect(self.update_row)
+
+    def update_row(self, index):
+        row = index.row()
+        id_row = self.table_model.item(row, 0).text()
+        self.upda = UpdateCategory(self.args[0], id_row)
+        self.upda.show()
+        self.hide()
+    def add_new_category(self):
+        self.add = AddCategory(self.args[0])
+        self.add.show()
+        self.hide()
+
+
     def load_data(self, data, column_data):
         column_data = [item[1] for item in column_data]
         self.table_model.setHorizontalHeaderLabels(column_data)
@@ -38,10 +52,121 @@ class CategoryData(QtWidgets.QMainWindow):
         for index_row, data_row in enumerate(data):
             for index_column, data_column in enumerate(column_data):
                 item = QStandardItem(str(data_row[index_column]))
-                item.setFlags(item.flags() ^ Qt.ItemIsEditable)
+                item.setFlags(item.flags() | Qt.ItemIsEnabled)
                 self.table_model.setItem(index_row, index_column, item)
 
     def exit(self):
         self.profile = main.Profile(self.args[0])
         self.profile.show()
+        self.hide()
+
+class AddCategory(QMainWindow):
+    def __init__(self, *args):
+        super(AddCategory, self).__init__()
+        self.args = args
+        uic.loadUi('Screens/category_supplier.ui', self)
+        self.setWindowTitle('Добавление новой категории')
+
+        self.label.setText('Добавить новую категорию')
+        self.pushButton.hide()
+        self.label_2.hide()
+        self.lineEdit.setPlaceholderText('Название категории')
+
+        self.pushButton_2.clicked.connect(self.add_category_data)
+        self.pushButton_3.clicked.connect(self.exit)
+
+    def add_category_data(self):
+        conn = Model().conn
+        cur = conn.cursor()
+        category_name = self.lineEdit.text()
+        if category_name == '':
+            self.label_2.setText('* Не все поля заполнены')
+            self.label_2.show()
+        else:
+            cur.execute("select CategoryID from Categories where CategoryName = ?", (category_name,))
+            if cur.fetchone() is None:
+                name = 'CategoryName'
+                try:
+                    cur.execute(f"INSERT INTO Categories({name}) VALUES ('{category_name}')")
+                    conn.commit()
+                    QMessageBox.information(self, 'Успех', 'Категория добавлена', QMessageBox.Ok)
+                    self.exit()
+                except conn.Error as er:
+                    QMessageBox.information(self, 'Провал', 'Ошибка добавления новой категории')
+            else:
+                self.label_2.setText('Такая категория уже есть')
+                self.label_2.show()
+
+    def exit(self):
+        self.dat = CategoryData(self.args[0])
+        self.dat.show()
+        self.hide()
+
+
+class UpdateCategory(QMainWindow):
+    def __init__(self, *args):
+        super(UpdateCategory, self).__init__()
+        self.args = args
+        uic.loadUi('Screens/category_supplier.ui', self)
+        self.setWindowTitle('Редактирование категории')
+
+        self.label.setText('Редактировать категорию')
+        self.label_2.hide()
+        self.lineEdit.setPlaceholderText('Название категории')
+
+        conn = Model().conn
+        cur = conn.cursor()
+        cur.execute(f"select * from Categories where CategoryID = {self.args[1]}")
+        data = cur.fetchone()
+        self.upload_data(data)
+        self.pushButton.clicked.connect(self.delete_category)
+        self.pushButton_2.clicked.connect(self.update_category_data)
+        self.pushButton_3.clicked.connect(self.exit)
+
+    def delete_category(self):
+        conn = Model().conn
+        cur = conn.cursor()
+        try:
+            cur.execute("""
+                            DELETE FROM Categories
+                            WHERE CategoryID = ?
+                        """, (self.args[1],))
+            conn.commit()
+            QMessageBox.information(self, 'Успех', 'Запись удалена', QMessageBox.Ok)
+            self.exit()
+        except conn.Error as er:
+            QMessageBox.information(self, 'Провал', 'Произошла ошибка добавления данных', QMessageBox.Ok)
+
+
+    def upload_data(self, data):
+        self.lineEdit.setText(data[1])
+    def update_category_data(self):
+        conn = Model().conn
+        cur = conn.cursor()
+        category_name = self.lineEdit.text()
+        if category_name == '':
+            self.label_2.setText('* Не все поля заполнены')
+            self.label_2.show()
+        else:
+            cur.execute("select CategoryID from Categories where CategoryName = ?", (category_name,))
+            if cur.fetchone() is None:
+                name = 'CategoryName'
+                try:
+                    cur.execute(f"""
+                        UPDATE Categories
+                            SET CategoryName = ?
+                            WHERE CategoryID = ?
+                    """, (category_name, int(self.args[1])))
+                    conn.commit()
+                    QMessageBox.information(self, 'Успех', 'Категория изменена', QMessageBox.Ok)
+                    self.exit()
+                except conn.Error as er:
+                    QMessageBox.information(self, 'Провал', 'Ошибка изменений категории')
+            else:
+                self.label_2.setText('Такая категория уже есть')
+                self.label_2.show()
+
+    def exit(self):
+        self.cate = CategoryData(self.args[0])
+        self.cate.show()
         self.hide()
--- Suppliers_view.py
+++ Suppliers_view.py
@@ -13,12 +13,11 @@ class SupplierData(QtWidgets.QMainWindow):
         uic.loadUi('Screens/table_data.ui', self)
         self.setWindowTitle('Категории')
         self.setMinimumSize(1120, 800)
-        self.label.setText('Поставщики')
+        self.label.setText('Категории')
         self.tableView = self.findChild(QtWidgets.QTableView, 'tableView')
         self.table_model = QStandardItemModel()
         self.tableView.setModel(self.table_model)
-        self.pushButton.hide()
-
+        self.pushButton.clicked.connect(self.add_new_supplier)
 
         conn = Model().conn
         cur = conn.cursor()
@@ -31,6 +30,21 @@ class SupplierData(QtWidgets.QMainWindow):
         self.load_data(data, column_data)
 
         self.pushButton_3.clicked.connect(self.exit)
+
+        self.tableView.doubleClicked.connect(self.update_row)
+
+    def update_row(self, index):
+        row = index.row()
+        id_row = self.table_model.item(row, 0).text()
+        self.upda = UpdateSupplier(self.args[0], id_row)
+        self.upda.show()
+        self.hide()
+    def add_new_supplier(self):
+        self.add = AddSupplier(self.args[0])
+        self.add.show()
+        self.hide()
+
+
     def load_data(self, data, column_data):
         column_data = [item[1] for item in column_data]
         self.table_model.setHorizontalHeaderLabels(column_data)
@@ -38,10 +52,121 @@ class SupplierData(QtWidgets.QMainWindow):
         for index_row, data_row in enumerate(data):
             for index_column, data_column in enumerate(column_data):
                 item = QStandardItem(str(data_row[index_column]))
-                item.setFlags(item.flags() ^ Qt.ItemIsEditable)
+                item.setFlags(item.flags() | Qt.ItemIsEnabled)
                 self.table_model.setItem(index_row, index_column, item)
 
     def exit(self):
         self.profile = main.Profile(self.args[0])
         self.profile.show()
+        self.hide()
+
+class AddSupplier(QMainWindow):
+    def __init__(self, *args):
+        super(AddSupplier, self).__init__()
+        self.args = args
+        uic.loadUi('Screens/category_supplier.ui', self)
+        self.setWindowTitle('Добавление нового поставщкика')
+
+        self.label.setText('Добавить нового поставщика')
+        self.pushButton.hide()
+        self.label_2.hide()
+        self.lineEdit.setPlaceholderText('Название поставщика')
+
+        self.pushButton_2.clicked.connect(self.add_supplier_data)
+        self.pushButton_3.clicked.connect(self.exit)
+
+    def add_supplier_data(self):
+        conn = Model().conn
+        cur = conn.cursor()
+        supplier_name = self.lineEdit.text()
+        if supplier_name == '':
+            self.label_2.setText('* Не все поля заполнены')
+            self.label_2.show()
+        else:
+            cur.execute("select SupplierID from Suppliers where SupplierName = ?", (supplier_name,))
+            if cur.fetchone() is None:
+                name = 'SupplierName'
+                try:
+                    cur.execute(f"INSERT INTO Suppliers({name}) VALUES ('{supplier_name}')")
+                    conn.commit()
+                    QMessageBox.information(self, 'Успех', 'Поставщик добавлен', QMessageBox.Ok)
+                    self.exit()
+                except conn.Error as er:
+                    QMessageBox.information(self, 'Провал', 'Ошибка добавления нового поставщика')
+            else:
+                self.label_2.setText('Такой поставщик уже есть')
+                self.label_2.show()
+
+    def exit(self):
+        self.dat = SupplierData(self.args[0])
+        self.dat.show()
+        self.hide()
+
+
+class UpdateSupplier(QMainWindow):
+    def __init__(self, *args):
+        super(UpdateSupplier, self).__init__()
+        self.args = args
+        uic.loadUi('Screens/category_supplier.ui', self)
+        self.setWindowTitle('Редактирование поставщика')
+
+        self.label.setText('Редактировать поставщика')
+        self.label_2.hide()
+        self.lineEdit.setPlaceholderText('Название поставщика')
+
+        conn = Model().conn
+        cur = conn.cursor()
+        cur.execute(f"select * from Suppliers where SupplierID = {self.args[1]}")
+        data = cur.fetchone()
+        self.upload_data(data)
+        self.pushButton.clicked.connect(self.delete_supplier)
+        self.pushButton_2.clicked.connect(self.update_supplier_data)
+        self.pushButton_3.clicked.connect(self.exit)
+
+    def delete_supplier(self):
+        conn = Model().conn
+        cur = conn.cursor()
+        try:
+            cur.execute("""
+                            DELETE FROM Suppliers
+                            WHERE SupplierID = ?
+                        """, (self.args[1],))
+            conn.commit()
+            QMessageBox.information(self, 'Успех', 'Запись удалена', QMessageBox.Ok)
+            self.exit()
+        except conn.Error as er:
+            QMessageBox.information(self, 'Провал', 'Произошла ошибка добавления данных', QMessageBox.Ok)
+
+
+    def upload_data(self, data):
+        self.lineEdit.setText(data[1])
+    def update_supplier_data(self):
+        conn = Model().conn
+        cur = conn.cursor()
+        supplier_name = self.lineEdit.text()
+        if supplier_name == '':
+            self.label_2.setText('* Не все поля заполнены')
+            self.label_2.show()
+        else:
+            cur.execute("select SupplierID from Suppliers where SupplierName = ?", (supplier_name,))
+            if cur.fetchone() is None:
+                name = 'SupplierName'
+                try:
+                    cur.execute(f"""
+                        UPDATE Suppliers
+                            SET SupplierName = ?
+                            WHERE SupplierID = ?
+                    """, (supplier_name, int(self.args[1])))
+                    conn.commit()
+                    QMessageBox.information(self, 'Успех', 'Поставщик изменен', QMessageBox.Ok)
+                    self.exit()
+                except conn.Error as er:
+                    QMessageBox.information(self, 'Провал', 'Ошибка изменений поставщика')
+            else:
+                self.label_2.setText('Такой поставщик уже есть')
+                self.label_2.show()
+
+    def exit(self):
+        self.cate = SupplierData(self.args[0])
+        self.cate.show()
         self.hide()
